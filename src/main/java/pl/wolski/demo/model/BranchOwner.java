f44227da2aac664dd41c270fa9381e428bc6c95f
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-07-30 17:12:09 +0200

    Add models

---
+++ src/main/java/pl/wolski/demo/model/BranchOwner.java
@@ -0,0 +1,15 @@
+package pl.wolski.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BranchOwner {
+
+    private String login;
+}
---
+++ src/main/java/pl/wolski/demo/model/RepositoryDetails.java
@@ -0,0 +1,19 @@
+package pl.wolski.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RepositoryDetails {
+
+    private String name;
+    private BranchOwner owner;
+    private List<BranchDetails> branches;
+}
